484e8ca48a362c28e87f7fe746b3d5ac47ac171f
Risic Thapliyal <91870209+RisicThapliyal@users.noreply.github.com>
2023-09-14 23:35:39 +0530

    Create Odd_Even.java

---
+++ Odd_Even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Conditional {
+
+    public static void main(Strings[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x % 2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
